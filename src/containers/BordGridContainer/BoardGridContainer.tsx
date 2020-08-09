a5f46c54cf67ac6a7a6f5909445f372fb4afd58e
Rokas <rokas.anisas@gmail.com>
2020-08-09 22:20:06 +0300

    [ GK-1 ] game action

--- src/containers/BordGridContainer/BoardGridContainer.tsx
+++ src/containers/BordGridContainer/BoardGridContainer.tsx
@@ -1,12 +1,15 @@
 import React, { FunctionComponent } from 'react';
 import { useSelector } from 'react-redux';
 
-import { UpdateGrid } from '@/store/actions';
+import { UpdateGrid, SetActivePlayer } from '@/store/actions';
 import { ApplicationState } from '@/store/types';
 import BoardGrid from '@/components/BoardGrid';
+import { Player } from '@/constants';
 
 const BoardGridContainer: FunctionComponent = () => {
-  const activePlayer = useSelector((state: ApplicationState) => state.activePlayer);
+  const activePlayer = useSelector(
+    (state: ApplicationState) => state.activePlayer
+  );
   const grid = useSelector((state: ApplicationState) => state.grid);
 
   const onItemClick = (id: number) => {
@@ -17,6 +20,9 @@ const BoardGridContainer: FunctionComponent = () => {
         player: activePlayer,
       },
     });
+
+    const nextPlayer = activePlayer === Player.o ? Player.x : Player.o;
+    SetActivePlayer(nextPlayer);
   };
 
   return <BoardGrid blocks={grid} onItemClick={onItemClick} />;
