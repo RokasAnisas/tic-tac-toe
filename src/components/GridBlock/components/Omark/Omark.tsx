ba1d4fa61bdc3df2781da4d251556a324fa2f8f3
Rokas <rokas.anisas@gmail.com>
2020-08-10 20:39:36 +0300

    [ GK-6 ] o mark animated

--- src/components/GridBlock/components/Omark/Omark.tsx
+++ src/components/GridBlock/components/Omark/Omark.tsx
@@ -7,7 +7,23 @@ const Omark: FunctionComponent = () => {
 
   return (
     <div className={className}>
-      <div className={`${className}__circle`} />
+      <svg
+        className={`${className}__svg`}
+        xmlns="http://www.w3.org/2000/svg"
+        width="100"
+        height="100"
+        viewBox="0 0 100 100"
+      >
+        <g>
+          <circle
+            className={`${className}__elipse`}
+            cx="50"
+            cy="50"
+            r="50"
+            strokeWidth={1.5}
+          />
+        </g>
+      </svg>
     </div>
   );
 };
