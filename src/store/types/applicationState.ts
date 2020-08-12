8f7fea3f5cce98331728c043b8c2798f051b5806
Rokas <rokas.anisas@gmail.com>
2020-08-12 21:11:39 +0300

    [ GK-4 ] initial state update + colors

--- src/store/types/applicationState.ts
+++ src/store/types/applicationState.ts
@@ -1,5 +1,6 @@
 import { GridBlockType, ConfirmDialogType, Score } from './';
 import { Player } from '@/constants';
+import { LogItem } from './logItem';
 
 export interface ApplicationState {
   activePlayer: Player;
@@ -7,4 +8,5 @@ export interface ApplicationState {
   confirmDialog: ConfirmDialogType;
   gridLock: boolean;
   score: Score;
+  actionLog?: Array<LogItem>;
 }
---
+++ src/store/types/logItem.ts
@@ -0,0 +1,6 @@
+import { Player } from "@/constants";
+
+export interface LogItem {
+  player?: Player;
+  message: string;
+}
