e72c415840ecc326e95979e9a8ce0a8a1c2bd5a2
Rokas <rokas.anisas@gmail.com>
2020-08-13 19:17:06 +0300

    [ GK-11 ] restore activePlayer

--- src/services/restoreSession.ts
+++ src/services/restoreSession.ts
@@ -1,7 +1,7 @@
-import { Storage } from '@/constants';
+import { Storage, Player } from '@/constants';
 import { storageUtil } from '@/utils';
 import { GridBlockType, LogItem } from '@/store/types';
-import { SetGrid, SetLog } from '@/store/actions';
+import { SetGrid, SetLog, SetActivePlayer } from '@/store/actions';
 
 export const restoreSession = (): void => {
   const grid = storageUtil.getItem(Storage.grid);
@@ -10,7 +10,11 @@ export const restoreSession = (): void => {
   const actionLog = storageUtil.getItem(Storage.actionLog);
   const actionLogParsed: LogItem[] = JSON.parse(actionLog!);
 
+  const activePlayer = storageUtil.getItem(Storage.activePlayer);
+  const activePlayerParsed: Player = JSON.parse(activePlayer!);
+
 
   SetGrid(gridParsed);
   SetLog(actionLogParsed);
+  SetActivePlayer(activePlayerParsed);
 };
