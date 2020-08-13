a0cb84f5e6dc3c447b5087d5c7c7b20ee0129202
Rokas <rokas.anisas@gmail.com>
2020-08-13 16:36:26 +0300

    [ GK-11 ] Storage util

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export { gridSize } from './gridSize';
 export { Player } from './player';
 export { Messages } from './messages';
+export { Storage } from './storage';
---
+++ src/constants/storage.ts
@@ -0,0 +1,5 @@
+export enum Storage {
+  score = 'score',
+  actionLog = 'actionLog',
+  grid = ''
+}
